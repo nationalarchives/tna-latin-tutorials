c5d8af0dfe3d1e28878eb96695d3db4f5d096637
Ashley Burnett (ADM) <a-aburnett@web.local>
2017-09-28 14:02:28 +0100

    Add meta, format code, cleanup

--- ash-html-template/menu.js
+++ ash-html-template/menu.js
@@ -1,39 +1,29 @@
-$(function() {
-
+$(function () {
     $("nav").hide();
     $("#search-selector").hide();
-    console.log( "hello!" );
 });
 
-$("#mega-menu-pull-down").click(function() {
-    if($("nav").is(":visible")){
+$("#mega-menu-pull-down").click(function () {
+    if ($("nav").is(":visible")) {
         $("nav").slideUp();
     }
     else {
         $("nav").slideDown();
     }
-
-
 });
 
-$("#scope-selector").click(function() {
-    if($("#search-selector").is(":visible")){
+$("#scope-selector").click(function () {
+    if ($("#search-selector").is(":visible")) {
         $("#search-selector").slideUp();
     }
     else {
         $("#search-selector").slideDown();
     }
-
-
-
 });
 
 $("#search-selector li a").click(function () {
-
     $("#globalSearch").attr("action", $(this).attr("data-target"));
-    $("#tnaSearch").attr("placeholder",$(this).attr("data-placeholder"));
+    $("#tnaSearch").attr("placeholder", $(this).attr("data-placeholder"));
     $("#search-selector").slideUp();
-
-
 });
 

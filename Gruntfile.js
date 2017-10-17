10a4a7cf1565a22cda27b8233a152e4ad491d25f
Ashley Burnett (ADM) <ashleyaburnett@gmail.com>
2017-10-17 15:57:05 +0100

    First version of working TNA Base Template

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            options: {
+                sourcemap: 'none'
+            },
+            dist: {
+                files: {
+                    'css/base-sass.css': 'css/sass/base-sass.scss'
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                sourceMap: true
+            },
+            target: {
+                files: {
+                    'css/base-sass.css.min': ['css/base-sass.css']
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: 'js/*.js',
+                tasks: ['qunit', 'concat', 'uglify']
+            },
+            css: {
+                files: 'css/sass/*.scss',
+                tasks: ['sass', 'cssmin']
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // Default task(s).
+    grunt.registerTask('default', ['sass', 'cssmin', 'watch']);
+
+};

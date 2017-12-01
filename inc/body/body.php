3fdd69046b1e6a1293fab2f150177e23af546c9f
AshTNA <ashleyaburnett@gmail.com>
2017-12-01 14:53:16 +0000

    Initial HTML/CSS based Landing Page

--- inc/body/body.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'inc/body/google-tag-manager.inc';
-include 'inc/body/tna-header.inc';
-include 'inc/body/tna-nav.php';
-?>
--- inc/head/head.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'header-meta-tags.php';
-include 'header-css.inc';
-include 'header-scripts.inc';
-?>
